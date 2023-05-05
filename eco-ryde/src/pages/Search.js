67fac4f6f8f5cfa538297abb320abe44392d55da
YounesLahrichi <Lahrichi.ys@gmail.com>
2023-05-05 14:46:17 -0700

    adding files

---
+++ eco-ryde/src/pages/Search.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtAv8VVIGEQizmG6xQaZFwSm6ByvA9uXI",
+  authDomain: "ecoryde-57f08.firebaseapp.com",
+  projectId: "ecoryde-57f08",
+  storageBucket: "ecoryde-57f08.appspot.com",
+  messagingSenderId: "414054055518",
+  appId: "1:414054055518:web:e7b29450a5d2374f10fecf"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
