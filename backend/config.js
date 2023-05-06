b92e5ea8f6e519c12cf0e99d9e4204570c46975a
Omar Shaban <78984955+omarshaban0@users.noreply.github.com>
2023-05-06 15:27:42 -0700

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,141 @@
+//import firebase from `firebase`;
+const firebase  = require("firebase")
+const firestore = require("firebase/firestore")
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtAv8VVIGEQizmG6xQaZFwSm6ByvA9uXI",
+  authDomain: "ecoryde-57f08.firebaseapp.com",
+  projectId: "ecoryde-57f08",
+  storageBucket: "ecoryde-57f08.appspot.com",
+  messagingSenderId: "414054055518",
+  appId: "1:414054055518:web:e7b29450a5d2374f10fecf"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+//const User = db.collection("User");
+//const Host = db.collection("Hosts");
+
+module.exports = db;
+//export const User = db.collection("User");
+
+/*
+//import  initializeApp  from "firebase/app";
+//import { getFirestore } from "firebase/firestore";
+const firebase = require("firebase");
+const firestore = require("firebase/firestore")
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyAtAv8VVIGEQizmG6xQaZFwSm6ByvA9uXI",
+    authDomain: "ecoryde-57f08.firebaseapp.com",
+    projectId: "ecoryde-57f08",
+    storageBucket: "ecoryde-57f08.appspot.com",
+    messagingSenderId: "414054055518",
+    appId: "1:414054055518:web:e7b29450a5d2374f10fecf"
+  };
+
+// Initialize Firebase
+const app = firebase(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+//const User = db.collection("User");
+//const Host = db.collection("Hosts");
+
+export const User = db.collection("User");
+*/
+
+/*
+//import  initializeApp  from "firebase/app";
+//import { getFirestore } from "firebase/firestore";
+const firebase = require("firebase");
+const firestore = require("firebase/firestore")
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyAtAv8VVIGEQizmG6xQaZFwSm6ByvA9uXI",
+    authDomain: "ecoryde-57f08.firebaseapp.com",
+    projectId: "ecoryde-57f08",
+    storageBucket: "ecoryde-57f08.appspot.com",
+    messagingSenderId: "414054055518",
+    appId: "1:414054055518:web:e7b29450a5d2374f10fecf"
+  };
+
+// Initialize Firebase
+const app = firebase(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+//const User = db.collection("User");
+//const Host = db.collection("Hosts");
+
+export const User = db.collection("User");
+*/
+
+/*
+//import  initializeApp  from "firebase/app";
+//import { getFirestore } from "firebase/firestore";
+const firebase = require("firebase");
+const firestore = require("firebase/firestore")
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyAtAv8VVIGEQizmG6xQaZFwSm6ByvA9uXI",
+    authDomain: "ecoryde-57f08.firebaseapp.com",
+    projectId: "ecoryde-57f08",
+    storageBucket: "ecoryde-57f08.appspot.com",
+    messagingSenderId: "414054055518",
+    appId: "1:414054055518:web:e7b29450a5d2374f10fecf"
+  };
+
+// Initialize Firebase
+const app = firebase(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+//const User = db.collection("User");
+//const Host = db.collection("Hosts");
+
+export const User = db.collection("User");
+*/
+
+/*
+//import  initializeApp  from "firebase/app";
+//import { getFirestore } from "firebase/firestore";
+const firebase = require("firebase");
+const firestore = require("firebase/firestore")
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyAtAv8VVIGEQizmG6xQaZFwSm6ByvA9uXI",
+    authDomain: "ecoryde-57f08.firebaseapp.com",
+    projectId: "ecoryde-57f08",
+    storageBucket: "ecoryde-57f08.appspot.com",
+    messagingSenderId: "414054055518",
+    appId: "1:414054055518:web:e7b29450a5d2374f10fecf"
+  };
+
+// Initialize Firebase
+const app = firebase(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+//const User = db.collection("User");
+//const Host = db.collection("Hosts");
+
+export const User = db.collection("User");
+*/
