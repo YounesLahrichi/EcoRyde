f4fd9fb29e58716547a6fd896906ad92dca570b0
anasazeemi <anas.azeemi02@gmail.com>
2023-05-06 00:26:24 -0700

    FactCards

---
+++ eco-ryde/src/components/FactCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function FactCard() {
+    return (
+        <div className="home-card">
+            <div className="notes-group-info">
+                <p>
+                    Title
+                </p>
+                <p>
+                    Filler
+                </p>
+                <p>
+                    Filler
+                </p>
+                <p>
+                    Filler
+                </p>
+            </div>
+        </div>
+    );
+}
--- eco-ryde/src/pages/Home.js
+++ eco-ryde/src/pages/Home.js
@@ -1,12 +1,21 @@
 import React, {useContext} from "react";
 import NavBarJSX from '../components/NavBar.jsx';
 import Footer from "../components/Footer.js";
+import FactCard from "../components/FactCard.js";
 
 export default function Home() {
     return (
         <div>
             <NavBarJSX/>
-            <h2>Home Page</h2>
+            <div>
+                <div>
+                    <h2>Home Page</h2>
+                </div>
+                <div className="home-card-container">
+                    <FactCard />
+                    <FactCard />
+                </div>
+            </div>
             <Footer />
         </div>
     )
