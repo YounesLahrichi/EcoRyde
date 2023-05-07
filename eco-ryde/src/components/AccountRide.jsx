8e28e08f8e006faff890dfb8596b009840e1e4e4
YounesLahrichi <Lahrichi.ys@gmail.com>
2023-05-06 22:02:57 -0700

    hi

--- eco-ryde/src/components/AccountRide.jsx
+++ eco-ryde/src/components/AccountRide.jsx
@@ -1,18 +1,11 @@
 import { useState, useEffect } from "react";
 import BookData from "../data.json"
 function AccountRide(){
-    const [data, setData] = useState([]);
-    setData(BookData);
 
-    console.log(data[0].author);
     return (
         <div>
             <h1 className="headerCenter">My Registered Rides</h1>
-            <ul>
-                {data.map((item) => (
-                <li >{item.author}</li>
-                ))}
-            </ul>
+            hi
         </div>
     )
 }
